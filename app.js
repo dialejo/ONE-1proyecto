e75150b66ed3bb3b17ba86c7a3ac3089e8d47605
dialejo <diegomaldonadom1992@gmail.com>
2025-01-18 09:48:50 -0500

    tips mayor o menor app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 //variables
-let numeroSecreto = 6;
+let numeroSecreto = 5;
 let numeroUsuario = prompt('¿Me indicas un numero por favor?');
-
 console.log(numeroUsuario);
 /*
 este codigo 
 realiza la comparacion
 */
+
 if (numeroSecreto == numeroUsuario) {
     //acertamos fue veradera la condicion
     alert(`Adivinaste el número es: ${numeroUsuario}`);
 }else{
+    if(numeroUsuario > numeroSecreto){
+        alert('El numero secreto es menor');
+    }else{
+        alert('el numero es mayor');
+    }
     //no se acerto la condicion
-    alert('No adivinaste el numero secreto');
+    //alert('No adivinaste el numero secreto');
 };
-
