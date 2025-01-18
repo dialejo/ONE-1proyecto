2ca5dc10d92d744ca893d8c9426a89f3a44d8980
dialejo <diegomaldonadom1992@gmail.com>
2025-01-18 08:41:26 -0500

    strings, variables, condiciones app

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+//variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt('¿Me indicas un numero por favor?');
 
 console.log(numeroUsuario);
+/*
+este codigo 
+realiza la comparacion
+*/
+if (numeroSecreto == numeroUsuario) {
+    //acertamos fue veradera la condicion
+    alert(`Adivinaste el número es: ${numeroUsuario}`);
+}else{
+    //no se acerto la condicion
+    alert('No adivinaste el numero secreto');
+};
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
-}
