0ab08fc38c2c5d8c3f60f6f27f2f5b9519d31ff0
dialejo <diegomaldonadom1992@gmail.com>
2025-01-18 20:42:32 -0500

    desafio cambio de rango

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 //variables
-let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let numeroMaximorango = parseInt(prompt('Indica el rango máximo para adivinar el número secreto enrte 1 y el rango que indiques:'));
+let numeroSecreto = Math.floor(Math.random() * numeroMaximorango) + 1;
 let numeroUsuario = 0;
 let contadorIntentos = 1;
-//let palabraIntento = 'intento';
-let MaximoIntentos = 3;
 
+if (numeroMaximorango <= 10) {
+    var MaximoIntentos = 3;
+}else if(numeroMaximorango <= 20){
+    var MaximoIntentos = 5;
+}else if(numeroMaximorango <= 50){
+    var MaximoIntentos = 7;
+}else if(numeroMaximorango <= 100){
+    var MaximoIntentos = 10;
+}else if(numeroMaximorango <= 200){
+    var MaximoIntentos = 15;
+}else if(numeroMaximorango <= 500){
+    var MaximoIntentos = 20;
+}else{
+    var MaximoIntentos = 30;
+};
+
+console.log(MaximoIntentos);
 while (numeroUsuario != numeroSecreto) {
-    let numeroUsuario = prompt('Me indicas un numero entre 1 y 10 por favor:');
-
-    console.log(numeroUsuario);
+    numeroUsuario = prompt(`Me indicas un numero entre 1 y ${numeroMaximorango} por favor:`);
+    numeroUsuario = parseInt(numeroUsuario);
+    console.log(typeof(numeroUsuario));
     if (numeroSecreto == numeroUsuario) {
         //acertamos fue veradera la condicion
         alert(`Adivinaste el número es: ${numeroUsuario}, lo hiciste en ${contadorIntentos} ${contadorIntentos ==1 ? 'intento' : 'intentos'}`);
@@ -19,9 +35,8 @@ while (numeroUsuario != numeroSecreto) {
         }else{
             alert('el numero es mayor');
         }
-        //incrmementamos el contador de intentos cuando no hacierta
+        //incrmeementamos el contador de intentos cuando no hacierta
         contadorIntentos++;
-        //palabraIntento = 'intentos';
         if(contadorIntentos > MaximoIntentos){
             alert(`llegaste al maximo de intentos que es ${MaximoIntentos} intentos y el numero secreto es ${numeroSecreto}`);
             break;
